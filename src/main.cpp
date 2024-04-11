27da52a0cfc0ef472de15035a48dbc94bd3d8ff5
TegranG <tegrgrigor@gmail.com>
2024-04-10 23:42:23 -0400

    jar temp implementation?

---
+++ src/main.cpp
@@ -0,0 +1,238 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       james                                                     */
+/*    Created:      Mon Aug 31 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+#include "ai_functions.h"
+
+using namespace vex;
+
+brain Brain;
+// Robot configuration code.
+motor leftDrive = motor(PORT1, ratio18_1, false);
+motor rightDrive = motor(PORT2, ratio18_1, true);
+gps GPS = gps(PORT18, 17, 14, distanceUnits::cm, 90.0, turnType::right);
+smartdrive Drivetrain = smartdrive(leftDrive, rightDrive, GPS, 319.19, 320, 40, mm, 1);
+motor Arm = motor(PORT3, ratio18_1, false);
+motor Intake = motor(PORT8, ratio18_1, false);
+
+Drive chassis(
+
+//Specify your drive setup below. There are eight options:
+//ZERO_TRACKER_NO_ODOM, ZERO_TRACKER_ODOM, TANK_ONE_ENCODER, TANK_ONE_ROTATION, TANK_TWO_ENCODER, TANK_TWO_ROTATION, HOLONOMIC_TWO_ENCODER, and HOLONOMIC_TWO_ROTATION
+//For example, if you are not using odometry, put ZERO_TRACKER_NO_ODOM below:
+ZERO_TRACKER_NO_ODOM,
+
+//Add the names of your Drive motors into the motor groups below, separated by commas, i.e. motor_group(Motor1,Motor2,Motor3).
+//You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2".
+
+//Left Motors:
+motor_group(),
+
+//Right Motors:
+motor_group(),
+
+//Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
+PORT1,
+
+//Input your wheel diameter. (4" omnis are actually closer to 4.125"):
+3.25,
+
+//External ratio, must be in decimal, in the format of input teeth/output teeth.
+//If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
+//If the motor drives the wheel directly, this value is 1:
+0.6,
+
+//Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
+//For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
+360,
+
+/*---------------------------------------------------------------------------*/
+/*                                  PAUSE!                                   */
+/*                                                                           */
+/*  The rest of the drive constructor is for robots using POSITION TRACKING. */
+/*  If you are not using position tracking, leave the rest of the values as  */
+/*  they are.                                                                */
+/*---------------------------------------------------------------------------*/
+
+//If you are using ZERO_TRACKER_ODOM, you ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
+
+//FOR HOLONOMIC DRIVES ONLY: Input your drive motors by position. This is only necessary for holonomic drives, otherwise this section can be left alone.
+//LF:      //RF:    
+PORT1,     -PORT2,
+
+//LB:      //RB: 
+PORT3,     -PORT4,
+
+//If you are using position tracking, this is the Forward Tracker port (the tracker which runs parallel to the direction of the chassis).
+//If this is a rotation sensor, enter it in "PORT1" format, inputting the port below.
+//If this is an encoder, enter the port as an integer. Triport A will be a "1", Triport B will be a "2", etc.
+3,
+
+//Input the Forward Tracker diameter (reverse it to make the direction switch):
+2.75,
+
+//Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
+//For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
+//This distance is in inches:
+-2,
+
+//Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
+1,
+
+//Sideways tracker diameter (reverse to make the direction switch):
+-2.75,
+
+//Sideways tracker center distance (positive distance is behind the center of the robot, negative is in front):
+5.5
+
+);
+
+// A global instance of competition
+competition Competition;
+
+// create instance of jetson class to receive location and other
+// data from the Jetson nano
+//
+ai::jetson  jetson_comms;
+
+
+/*----------------------------------------------------------------------------*/
+// Create a robot_link on PORT1 using the unique name robot_32456_1
+// The unique name should probably incorporate the team number
+// and be at least 12 characters so as to generate a good hash
+//
+// The Demo is symetrical, we send the same data and display the same status on both
+// manager and worker robots
+// Comment out the following definition to build for the worker robot
+#define  MANAGER_ROBOT    1
+
+#if defined(MANAGER_ROBOT)
+#pragma message("building for the manager")
+ai::robot_link       link( PORT11, "robot_32456_1", linkType::manager );
+#else
+#pragma message("building for the worker")
+ai::robot_link       link( PORT11, "robot_32456_1", linkType::worker );
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                          Auto_Isolation Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous isolation  */
+/*  phase of a VEX AI Competition.                                           */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void auto_Isolation(void) {
+  // Calibrate GPS Sensor
+  GPS.calibrate();
+  // Optional wait to allow for calibration
+  wait(1,sec);
+  // Finds and moves robot to position of closest green triball
+  getObject();
+  // Intakes the ball
+  double rot = Arm.position(rotationUnits::deg);
+  intake(rot - 100, 1);
+  // Moves to position in front of blue goal
+  wait(1,sec);
+  goToGoal(0);
+  // Scores tri-ball in blue goal
+  dump(rot);
+
+}
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                        Auto_Interaction Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous interaction*/
+/*  phase of a VEX AI Competition.                                           */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+
+void auto_Interaction(void) {
+  // Functions needed: evaluate which ball detected is target, go to target (x,y), intake ball, dump ball, 
+}
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                          AutonomousMain Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+bool firstAutoFlag = true;
+
+void autonomousMain(void) {
+  // ..........................................................................
+  // The first time we enter this function we will launch our Isolation routine
+  // When the field goes disabled after the isolation period this task will die
+  // When the field goes enabled for the second time this task will start again
+  // and we will enter the interaction period. 
+  // ..........................................................................
+
+  if(firstAutoFlag)
+    auto_Isolation();
+  else 
+    auto_Interaction();
+
+  firstAutoFlag = false;
+}
+
+int main() {
+
+  // local storage for latest data from the Jetson Nano
+  static AI_RECORD       local_map;
+
+  // Run at about 15Hz
+  int32_t loop_time = 33;
+
+  // start the status update display
+  thread t1(dashboardTask);
+
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomousMain);
+
+  // print through the controller to the terminal (vexos 1.0.12 is needed)
+  // As USB is tied up with Jetson communications we cannot use
+  // printf for debug.  If the controller is connected
+  // then this can be used as a direct connection to USB on the controller
+  // when using VEXcode.
+  //
+  //FILE *fp = fopen("/dev/serial2","wb");
+  this_thread::sleep_for(loop_time);
+
+  Arm.setStopping(hold);
+  Arm.setVelocity(60, percent);
+
+  while(1) {
+      // get last map data
+      jetson_comms.get_data( &local_map );
+
+      // set our location to be sent to partner robot
+      link.set_remote_location( local_map.pos.x, local_map.pos.y, local_map.pos.az, local_map.pos.status );
+
+      // fprintf(fp, "%.2f %.2f %.2f\n", local_map.pos.x, local_map.pos.y, local_map.pos.az)
+
+      // request new data    
+      // NOTE: This request should only happen in a single task.    
+      jetson_comms.request_map();
+
+      // Allow other tasks to run
+      this_thread::sleep_for(loop_time);
+  }
+}
